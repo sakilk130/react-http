289e37629f33765a003b8e815ec55e24314aaae0
sakilk130 <sakilk130@gmail.com>
2020-10-05 13:17:46 +0600

    GetList

---
+++ src/components/get/GetList.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class GetList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      get: [],
+      error: '',
+    };
+  }
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/post')
+      .then((responce) => {
+        this.setState({
+          get: responce.data,
+        });
+        // console.log(this.state.get);
+      })
+      .catch((error) => {
+        this.setState({
+          error: 'Something Wrong',
+        });
+        // console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Get-List</h2>
+        <h2>{this.state.error}</h2>
+        <ol>
+          {this.state.get.map((gets, id) => (
+            <li key={id}>
+              <i>{gets.title}</i>{' '}
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default GetList;
